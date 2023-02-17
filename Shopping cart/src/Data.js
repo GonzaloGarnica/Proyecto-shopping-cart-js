6bb78ef301ea4c314363bbe0ebf6e9408c3ac895
GonzaloGarnica <114120343+GonzaloGarnica@users.noreply.github.com>
2023-02-17 15:34:07 -0300

    Add files via upload

---
+++ Shopping cart/src/Data.js
@@ -0,0 +1,83 @@
+
+let shopItemsData= [{
+    id:"camisacasual",
+    name:"Camisa casual",
+    price: 4500,
+    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img:"images/img-1.jpg"
+},
+{ 
+    id:"camisaoficina",
+    name:"Camisa de oficina",
+    price: 8500,
+    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img:"images/img-2.jpg"
+},{ 
+    id:"camiseta",
+    name:"Camiseta masculina",
+    price: 6000,
+    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img:"images/img-3.jpg"
+},{
+    id:"traje",
+    name:"Traje de hombre",
+    price: 75000,
+    desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img:"images/img-4.jpg"
+
+},  {
+    id: "corbatahombre",
+    name: "Corbata masculina",
+    price: 2500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.png",
+  },
+  {
+    id: "zapatoscasuales",
+    name: "Zapatillas",
+    price: 20000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.png",
+  },
+  {
+    id: "trajenegro",
+    name: "Traje negro",
+    price: 45000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.png",
+  },
+  {
+    id: "camisetapolo",
+    name: "Camiseta polo",
+    price: 4500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.png",
+  },
+  {
+    id: "camisajean",
+    name: "Camisa de jean",
+    price: 8500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-9.png",
+  },
+  {
+    id: "pantalonjean",
+    name: "Pantalón jean",
+    price: 12000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-10.png",
+  },
+  {
+    id: "gorrabasic",
+    name: "Gorra básica",
+    price: 3500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-11.png",
+  },
+  {
+    id: "botascuero",
+    name: "Botas de cuero",
+    price: 35000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-12.png",
+  },]
